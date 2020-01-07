119c19e076a9eae67407d0d58e8ca739c1e380d8
Aboelmagd <Developer.ahmed@Gmail.com>
2020-01-07 17:50:51 +0200

    add .net core console app

---
+++ IronPdfSamplesSolution/ConvertUrlToPdf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConvertUrlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var render = new IronPdf.HtmlToPdf();
+            var doc = render.RenderUrlAsPdf("https://www.wikipedia.org/");
+            doc.SaveAs($@"{AppDomain.CurrentDomain.BaseDirectory}\wiki.pdf");
+        }
+    }
+}
