d60b7f9cc618eaaa1132ed050d52d920fd6b3e6e
Aboelmagd <Developer.ahmed@Gmail.com>
2020-01-07 19:04:20 +0200

    add html string render

--- IronPdfSamplesSolution/ConvertUrlToPdf/Program.cs
+++ IronPdfSamplesSolution/ConvertUrlToPdf/Program.cs
@@ -6,10 +6,22 @@ namespace ConvertUrlToPdf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var render = new IronPdf.HtmlToPdf();
+            var doc = render.RenderHtmlAsPdf("<h1>Hello IronPdf</h1>");
+            doc.SaveAs($@"{AppDomain.CurrentDomain.BaseDirectory}\HtmlString.pdf");
+        }
+        static void RenderUrlToPdf()
+        {
             var render = new IronPdf.HtmlToPdf();
             var doc = render.RenderUrlAsPdf("https://www.wikipedia.org/");
             doc.SaveAs($@"{AppDomain.CurrentDomain.BaseDirectory}\wiki.pdf");
+        }
+
+        static void RenderHtmlStringToPdf()
+        {
+            var render = new IronPdf.HtmlToPdf();
+            var doc = render.RenderHtmlAsPdf("<h1>Hello IronPdf</h1>");
+            doc.SaveAs($@"{AppDomain.CurrentDomain.BaseDirectory}\HtmlString.pdf");
         }
     }
 }
