803783c9d669b7ecb4a2b6f1eef516a6218aa502
ahmed <ahmed@DESKTOP-A9M6ER9>
2020-01-23 21:06:40 +0200

    updating document

---
+++ IronXLSamplesSolution/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workbook = IronXL.WorkBook.Load(@"D:\EbtkarSolutions\IronSoftware\Tutorials\IronSoftware\IronXLSamplesSolution\Documents\Files\CSVList.csv");
+            var sheet = workbook.WorkSheets.First();
+            var cell = sheet["A1"].StringValue;
+            Console.WriteLine(cell);
+        }
+
+        static void LoadXlsx()
+        {
+            var workbook = IronXL.WorkBook.Load(@"D:\EbtkarSolutions\IronSoftware\Tutorials\IronSoftware\IronXLSamplesSolution\Documents\Files\HelloWorld.xlsx");
+            var sheet = workbook.WorkSheets.First();
+            var cell = sheet["A1"].StringValue;
+            Console.WriteLine(cell);
+        }
+    }
+}
